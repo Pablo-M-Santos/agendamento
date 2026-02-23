311dda76f4ca5bd2339ee3a3b2da88b81c32f5b3
Pablo Santos <pablomoreirasantos.hp@gmail.com>
2026-02-23 17:06:05 -0300

    feat: add toast notification and email verification pages

---
+++ app/pages/teste.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const toast = useToast()
+
+function showToast() {
+  toast.add({
+    title: 'Funcionando!',
+    description: 'Agora sim 🎉'
+  })
+}
+</script>
+
+<template>
+  <div class="p-10">
+    <UButton label="Show toast" color="neutral" variant="outline" @click="showToast" />
+  </div>
+</template>
